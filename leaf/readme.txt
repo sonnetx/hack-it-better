0570736dbd708842d7e1ba640b7d651bc0c9b24b
Sonnet Xu <sonnet.xu@gmail.com>
2020-10-04 11:08:45 +0000

    added new pages

---
+++ leaf/readme.txt
@@ -0,0 +1,22 @@
+﻿=== Leaf Cursor Set ===
+
+By: Spaceman
+
+Download: http://www.rw-designer.com/cursor-set/leaf
+
+Author's description:
+
+For "TOTM Contest - April".
+
+==========
+
+License: Creative Commons - Noncommercial
+
+You are free:
+
+* To Share - To copy, distribute and transmit the work.
+* To Remix - To adapt the work.
+
+Under the following conditions:
+
+* Noncommercial - You may not use this work for commercial purposes.
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-<script>
-var location = document.getElementById("position");
+//var location = document.getElementById("position");
 function GPScoords() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(dropCoords);
@@ -17,19 +16,30 @@ function dropCoords(position) {
 
 function calc_emissions(time, type) {
   if (type == 3) {
-    window.alert("Good job! By walking or biking, you're not producing carbon emmissions! Keep up the good work!");
+    window.alert("Good job! By walking or biking, you're not producing carbon emmissions or any greenhouse gas! Keep up the good work!");
+  }
+  else if (type == 1) {
+    var g_emmisions = time * 150;
+    window.alert("Try to reduce how much you use your car! By driving your car that distance, you have output approximately " + g_emmisions + " grams of CO2");
   }
   else if (type == 2) {
-    var g_emmisions = time * 150;
-    window.alert("Try to reduce how much you use your car! By driving your car that distance, you have output approximately", g_emmisions, "grams of CO2");
+    var g_emmisions = time * 75;
+    window.alert("Good job on not using cars, you're helping out the planet a lot! If possible, try to start walking and biking as greener alternatives, keep up the good work! By riding the bus that distance, you've output about " + g_emmisions + " grams of CO2")
+  }
+  else {
+    window.alert("Double check that you're looking at the key provided for the methods of transport, the value you entered is not recognized.")
   }
 }
 
-function calculate {
+function calculate() {
   var time = document.getElementById("minutes").value;
-  console.log(time);
+  //window.alert(time);
   var method = document.getElementById("transport").value;
-  console.log(method);
+  //window.alert(method);
   calc_emissions(time, method);
+  //window.alert('hi')
 }
-</script> 
+
+function displaySubmission() {
+  window.alert("Thank you for taking time out of your day to contact us, we will get in touch with you as soon as possible.");
+}
